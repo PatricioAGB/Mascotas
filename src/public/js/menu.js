a2ffb14d38e39216291c1d0e05b7500af1111d71
Blackfort1 <82190437+Blackfort1@users.noreply.github.com>
2021-12-03 20:34:05 -0300

    ACTUALIZACION CSS Y INDEX

---
+++ src/public/js/menu.js
@@ -0,0 +1,9 @@
+const harmburger = document.querySelector ('.hamburger');
+const menu = document.querySelector ('.menu-navegacion');
+
+console.log(menu)
+console.log(harmburger)
+
+harmburger.addEventListener('click', ()=>{
+    alert("click")
+})
