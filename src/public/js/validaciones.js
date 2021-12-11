99e179878b7280154a3f47929b2cce59c81c329c
Razer81368 <Razer81368@gmail.com>
2021-12-11 01:42:53 -0300

    Login registro listo

--- src/public/js/validaciones.js
+++ src/public/js/validaciones.js
@@ -72,6 +72,41 @@ function validar(texto) {
    return bandera;
 };
 
+function verificarPasswords() {
+ 
+   // Obtenemos los valores de los campos de contraseñas 
+   pass1 = document.getElementById('password');
+   pass2 = document.getElementById('password1');
+   console.log(pass1);
+   console.log(pass2);
+   // Verificamos si las constraseñas no coinciden 
+   if (pass1.value != pass2.value) {
+
+       // Si las constraseñas no coinciden mostramos un mensaje 
+       document.getElementById("error").classList.add("mostrar");
+
+       return false;
+   } else {
+
+       // Si las contraseñas coinciden ocultamos el mensaje de error
+       document.getElementById("error").classList.remove("mostrar");
+
+       // Mostramos un mensaje mencionando que las Contraseñas coinciden 
+       document.getElementById("ok").classList.remove("ocultar");
+
+       // Desabilitamos el botón de login 
+       document.getElementById("login").disabled = true;
+
+       // Refrescamos la página (Simulación de envío del formulario) 
+       setTimeout(function() {
+           location.reload();
+       }, 3000);
+
+       return true;
+   }
+
+}
+
 function validarFormulario() {
    //Se declaran las variable correspondientes
    let nombre = document.getElementById('NOMBRE');
